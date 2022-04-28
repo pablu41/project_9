aeb77e9380e44cb2a57f897399c147e15ca29445
pablu41 <100160123+pablu41@users.noreply.github.com>
2022-04-28 16:36:48 +0600

    assignment 9

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+      // isset 
+    //  $name = 'Pablu';
+
+    // if (isset($name)) {
+    //      echo "Yes value already set";
+    //  } else {
+    //      echo "No value not set";
+    //  }
+    //  echo "<br>";
+
+
+    // // Empty
+    //  $value = '';
+    //  if (empty($value)) {
+    //      echo "Value is empty";
+    //  } else {
+    //      echo "Value is already have";
+    //  }
+    //  echo "<br>";
+
+    // //  die 
+    //     echo "saleh ahmed pablu";
+        
+    //     die();
+    //     echo "Rafikul islam";
+   
+    
+
+    // // undefined variable
+    // $value1;
+    // echo $value1;
+    // info();
+
+
+    // empty /flag variable
+    $list1 = '';
+    $list1 .= '<ul>';
+    $list1 .= '<li>list number 1 </li>';
+    $list1 .= '<li>list number 2 </li>';
+    $list1 .= '<li>list number 3 </li>';
+    $list1 .= '</ul>';
+
+    echo $list1;
+
+    
+   
+
+ ?>
